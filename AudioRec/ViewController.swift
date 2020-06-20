c2e8c27a58713ee88ab44696fdb1cf0643a9f11b
benkirbycodes <benkirbycodes@gmail.com>
2020-06-20 09:49:45 -0600

    Initial Commit

---
+++ AudioRec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioRec
+//
+//  Created by Ben Kirby on 6/20/20.
+//  Copyright © 2020 Ben Kirby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
